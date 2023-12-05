dbb99539f03de23270c37c5148b372f6dd5ce839
KianDi <99605246+KianDi@users.noreply.github.com>
2023-12-05 16:20:42 -0500

    Add files via upload
    A few of the first functions filled out for the first class.

---
+++ testing.py
@@ -0,0 +1,26 @@
+class SudokuGenerator:
+    def __init__(self, row_length, removed_cells):
+        self.row_length = row_length
+        self.removed_cells = removed_cells
+        self.board = [[0] * row_length for _ in range(row_length)]
+        self.box_length = int(math.sqrt(row_length))
+
+    def get_board(self):
+        return self.board
+
+    def print_board(self):
+        for row in self.board:
+            print(row)
+
+    def valid_in_row(self, row, num):
+        return num not in self.board[row]
+
+    def valid_in_col(self, col, num):
+        return num not in [row[col] for row in self.board]
+
+    def valid_in_box(self, row_start, col_start, num):
+        for i in range(self.box_length):
+            for j in range(self.box_length):
+                if self.board[row_start + i][col_start + j] == num:
+                    return False
+        return True
