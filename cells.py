cbcb81815e53f9d8c585236acdef48a629168632
AidenDonohoe <aidendonohoe@ufl.edu>
2023-12-05 16:55:33 -0500

    adding cells class file

---
+++ cells.py
@@ -0,0 +1,21 @@
+class Cell:
+
+    sketched_value = 0
+    def __init__(self, value, row, col, screen):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.screen = screen
+
+    def set_cell_value(self, value):
+        self.value = value
+
+    def set_sketched_value(self, value):
+        self.sketched_value = value
+
+    def draw(self):
+        if self.sketched_value != 0:
+            print(self.sketched_value)
+
+
+
