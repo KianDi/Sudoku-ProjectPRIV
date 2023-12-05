fd55575b02d93640ae998fe3d8b8f06e24e53918
AidenDonohoe <aidendonohoe@ufl.edu>
2023-12-05 17:44:57 -0500

    changed file name

--- cells.py
+++ cells.py
@@ -14,10 +14,12 @@ class Cell:
         self.sketched_value = value
 
     def draw(self):
+        center = (self.col * 100-50, self.row*100-50)
+        corner = (self.col * 100-75, self.row*100-75)
         if self.value == 0 and self.sketched_value != 0:
-            print(self.sketched_value)
+            self.screen.blit(self.sketched_value, corner)
         elif self.value != 0:
-            print(self.value)
+            self.screen.blit(self.value, center)
 
 
 
